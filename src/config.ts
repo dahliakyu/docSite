c0a174289515da5464554e082e4cf6429c171c39
dahliakyu <lamiyavi@hotmail.com>
2023-01-28 00:27:17 +0100

    updated blog for today "))))))))))))))))

--- src/config.ts
+++ src/config.ts
@@ -16,6 +16,16 @@ export const LOGO_IMAGE = {
   width: 216,
   height: 46,
 };
+export type Sidebar = Record<string, { text: string; link: string }[]>;
+
+export const SIDEBAR: Sidebar = {
+  "Section Header": [
+    { text: "Introduction", link: "en/introduction" },
+    { text: "Page 2", link: "en/page-2" },
+    { text: "Page 3", link: "en/page-3" },
+  ],
+  "Another Section": [{ text: "Page 4", link: "en/page-4" }],
+};
 
 export const SOCIALS: SocialObjects = [
   {
